4fabe7196f2a34d8b147bdc3a09744b4babe3f1a
vitkondz <vitanhv26@gmail.com>
2022-11-07 10:37:44 +0700

    Oneal follow bomber when get close

--- src/main/java/entity/animated/mob/Oneal.java
+++ src/main/java/entity/animated/mob/Oneal.java
@@ -1,39 +1,126 @@
 package entity.animated.mob;
 
+import entity.animated.Bomb;
+import entity.tile.Tile;
 import javafx.scene.image.Image;
 import mainClass.Board;
 import sprite.Sprite;
 
 public class Oneal extends Enemy {
 
+  private boolean follow = false;
+
+  private long timeFollow = 0;  // khi chạm bom sẽ tự né
+
   public Oneal(int x, int y) {
     super(x, y);
+    velocityX = 1;
   }
-
 
   @Override
   public Image getImage() {
     if (alive) {
-      return Sprite.oneal[direction * 3 + (int) (moving * Board.frame / 20)];
+      return Sprite.oneal[direction % 2 * 3 + (int) (moving * Board.frame / 20)];
     } else {
       return Sprite.oneal_dead;
     }
   }
 
-
   @Override
   public void calculateMove() {
-    // TODO Auto-generated method stub
 
+    int enemyX = (x + 16) / 32;
+    int enemyY = (y + 16) / 32;
+    int bomberX = (Board.getBomber().getX() + 12) / 32;
+    int bomberY = (Board.getBomber().getY() + 16) / 32;
+
+    // khoảng cách gần thì sẽ đuổi, nếu không thì đi random (đang cho đi ngang)
+    if (Math.pow(enemyX - bomberX, 2) + Math.pow(enemyY - bomberY, 2) < 50) {
+      timeFollow--;
+      if (timeFollow < 0) {
+        follow = true;
+      }
+      if (timeFollow < -7500) {
+        timeFollow = -1;
+      }
+    } else {
+      follow = false;
+    }
+
+    if (follow) {
+      if (enemyX > bomberX) {
+        direction = 0;
+        velocityX = -1;
+      } else if (enemyX < bomberX) {
+        direction = 1;
+        velocityX = 1;
+      }
+      if (enemyY > bomberY) {
+        direction = 2;
+        velocityY = -1;
+      } else if (enemyY < bomberY) {
+        direction = 3;
+        velocityY = 1;
+      }
+    }
+
+    move();
   }
 
 
   @Override
   protected void move() {
-    // TODO Auto-generated method stub
+    moving = 1;
+    x += velocityX;
+    for (Tile i : Board.getTileList()) {
+      if (isCollidedWith(i)) {
+        x -= velocityX;
+        if (!follow) {
+          velocityX = -velocityX;
+        }
+        break;
+      }
 
+      for (Bomb j : Board.getBombList()) {
+        if (isCollidedWith(j)) {
+          x -= velocityX;
+          setFollow(false);
+          velocityX = -velocityX;
+          break;
+        }
+      }
+
+    }
+
+    y += velocityY;
+    for (Tile i : Board.getTileList()) {
+      if (isCollidedWith(i)) {
+        y -= velocityY;
+        if (!follow) {
+          velocityY = -velocityY;
+        }
+        break;
+      }
+
+      for (Bomb j : Board.getBombList()) {
+        if (isCollidedWith(j)) {
+          y -= velocityY;
+          setFollow(false);
+          velocityY = -velocityY;
+          break;
+        }
+      }
+    }
   }
 
+  public void setFollow(boolean b) {
+    if (!b) {
+      follow = false;
+      timeFollow = 70;
+    } else {
+      follow = true;
+    }
+  }
 
   @Override
   public void update() {
@@ -43,6 +130,5 @@ public class Oneal extends Enemy {
     } else {
       die();
     }
-
   }
 }
